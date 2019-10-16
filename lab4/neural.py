70551103b67637e2d6242476aa4eaa665273b56c
Кирилл Терентьев <kirillterentyev1999@gmail.com>
2019-10-16 23:03:04 +0300

    some fixes

--- lab4/neural.py
+++ lab4/neural.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+
 def draw_points(x_arr, y_arr, *args):
     plt.plot(x_arr, y_arr, *args)
 
@@ -26,32 +27,6 @@ def draw_line(x_arr, m, c, *args):
 
 def get_error(t, y):
     return t - y
-
-
-def neural(x_arr, y_arr, m_init, c_init, n_coef, e_min, n_max):
-    m = np.random.uniform(-10, 10)
-    c = np.random.uniform(-10, 10)
-    e_arr = np.array([])
-    n = 1  # outer iteration counter
-    while True:
-        error = 0
-        for i in range(len(x_arr)):
-            dy = get_error(y_arr[i], m * x_arr[i] + c)  # сигма, ошибка
-            dm = dy * x_arr[i] * n_coef
-            dc = dy * n_coef
-            m += dm
-            c += dc
-            error += get_error(y_arr[i], m * x_arr[i] + c)
-
-        e_arr = np.append(e_arr, abs(error))
-        update_plot(x_arr, y_arr, m_init, c_init, m, c, n)
-        n += 1
-        if e_arr[-1] <= e_min or n > n_max:   # e <= e_min
-            break
-
-    plt.text(0, 21, 'Finish')
-    plt.show()
-    return [m, c, e_arr]
 
 
 def init_plot(x_arr, y_arr, m, c):
@@ -85,13 +60,43 @@ def draw_errors(e_arr, e_min):
     plt.clf()
     plt.grid(True)
     plt.plot(e_arr)
-    plt.axhline(e_min, 0, len(e_arr), color='red')
+    p1 = plt.axhline(e_min, 0, len(e_arr), color='red')
+    plt.legend([p1], ['Least squares method error'])
+    plt.xlabel('Iterations')
+    plt.ylabel('Error')
+
+
+def neural(x_arr, y_arr, m_init, c_init, n_coef, e_min, n_max):
+    m = np.random.uniform(-50, 50)
+    c = np.random.uniform(-50, 50)
+    e_arr = np.array([])
+    n = 1  # outer iteration counter
+    while True:
+        error = 0
+        for i in range(len(x_arr)):
+            dy = get_error(y_arr[i], m * x_arr[i] + c)  # сигма, ошибка
+            dm = dy * x_arr[i] * n_coef
+            dc = dy * n_coef
+            print("dm={0}; dc={1}".format(dm, dc))
+            m += dm
+            c += dc
+            error += get_error(y_arr[i], m * x_arr[i] + c)
+
+        e_arr = np.append(e_arr, abs(error))
+        update_plot(x_arr, y_arr, m_init, c_init, m, c, n)
+        n += 1
+        if e_arr[-1] <= e_min or n > n_max:
+            break
+
+    plt.text(0, 21, 'Finish')
+    plt.show()
+    return [m, c, e_arr]
 
 
 def main():
     x_arr = np.array([i + 1 for i in range(9)])
     y_arr = np.array([5.8, 7.8, 8, 9.4, 10.2, 11, 12, 13, 14.4])
-    n_coef = 0.001
+    n_coef = 0.01
     n_max = 300
     m_init, c_init, e_min = least_squares(x_arr, y_arr)
     print("Min Error={0}".format(e_min))
