7966be0d004b28c02fb081503a9e14f9633d89fd
terentyevkirill <kirillterentyev1999@gmail.com>
2019-12-05 23:24:00 +0200

    1,2,3 parts are done

--- lab8/row_model.py
+++ lab8/row_model.py
@@ -1,7 +1,10 @@
 import numpy as np
 import math
+from math import exp
 import constants as const
+from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy.linalg as lng
 
 
@@ -208,7 +211,7 @@ def with_noise(s1, s2, s3):
     plt.show()
 
     plt.figure()
-    plt.plot(const.arange, s2, color="red")
+    # plt.plot(const.arange, s2, color="red")
     plt.plot(const.arange, s22, color="blue")
     plt.xlabel("t")
     plt.ylim([0, 100])
@@ -217,7 +220,7 @@ def with_noise(s1, s2, s3):
     plt.show()
 
     plt.figure()
-    plt.plot(const.arange, s3, color="red")
+    # plt.plot(const.arange, s3, color="red")
     plt.ylim([0, 100])
     plt.plot(const.arange, s33, color="yellow")
     plt.xlabel("t")
@@ -235,32 +238,115 @@ def with_noise(s1, s2, s3):
 
     e33 = get_error(s3, matrix_x_s33[0], matrix_x_s33[1], matrix_x_s33[2], matrix_x_s33[3])
     min_e3 = e33
-    i_min_e3 = np.argmax([e31, e32, e33])
+    i_min_e3 = 2
 
     print(f"e11={e11}, e22={e22}, e33={e33}")
 
     plt.figure()
-    plt.plot(const.arange, res_s1, color="red")
-    plt.plot(const.arange, res_s2, color="green")
-    plt.plot(const.arange, res_s3, color="blue")
+    plt.plot(const.arange, s11, color="red")
+    plt.plot(const.arange, s22, color="green")
+    plt.plot(const.arange, s33, color="blue")
     plt.legend([f"s1{i_min_e1 + 1}", f"s2{i_min_e2 + 1}", f"s3{i_min_e3 + 1}"])
     plt.ylim([0, 100])
     plt.xlabel("t")
-    plt.ylabel("result s")
+    plt.ylabel("result s (noised)")
     plt.show()
 
 
+# s4(t)=exp(-bt)+exp(-ct).
+def s4(b, c, t):
+    return exp(-b * t) + exp(-c * t)
 
+def exponential():
+    b = const.b
+    c = const.c
+    array_b = np.arange(b - 1, b + 1, 0.0625)
+    nb = len(array_b)
+    array_c = np.arange(c - 2, c + 2, 0.125)
+    nc = len(array_c)
+    array_t = np.arange(1, 10, 1)
+    E = np.zeros([nb, nc])
+    for i in range(nb):
+        for j in range(nc):
+            E_ti = 0
+            bi = array_b[i]
+            ci = array_c[j]
+            for ti in array_t:
+                fbc = s4(bi, ci, ti)
+                ft = s4(b, c, ti)
+                E_ti += pow(ft - fbc, 2)
+            E[i, j] = E_ti
 
+    # Emin
+    Emin = np.amin(E)
+    pos_min = np.where(E == np.amin(E))
+    i_min = pos_min[0][0]
+    j_min = pos_min[1][0]
+
+    # Emax
+    Emax = np.amax(E)
+    pos_max = np.where(E == np.amax(E))
+    i_max = pos_max[0][0]
+    j_max = pos_max[1][0]
+
+    # Plot a 3D surface
+    fig = plt.figure(figsize=(9, 9))
+    ax = fig.add_subplot(111, projection='3d')
+    surf = ax.plot_surface(array_b, array_c, E, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+    ax.set_xlabel('b')
+    ax.set_ylabel('c')
+    ax.set_zlabel('E')
+    ax.set_xlim(b - 1.2, b + 1.2)
+    ax.set_ylim(c - 2.2, c + 2.2)
+    ax.set_zlim(Emin, Emax)
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
+
+    # dots in surface
+    fig = plt.figure(figsize=(9, 9))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(array_b, array_c, E, cmap='Purples_r')
+    ax.set_xlabel('b')
+    ax.set_ylabel('c')
+    ax.set_zlabel('E')
+    ax.set_zlim(Emin, Emax)
+    ax.scatter3D(array_b[i_min], array_c[j_min], Emin, c='red', marker='o')
+    ax.scatter3D(array_b[i_max], array_c[j_max], Emax, c='black', marker='o')
+    # Edots
+    for i in range(1, i_min):
+        ax.scatter3D(array_b[i], array_c[i], E[i, i], s=20, c='black', marker='o')
+    plt.title('Surface E')
+
+    # Plot a 3D scontour
+
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.contour(array_b, array_c, E, 200, origin='lower', cmap='Purples_r', linewidths=2, extent=(-3, 3, -2, 1))
+    plt.plot(array_b[i_min], array_c[j_min], c='red', marker='o')
+    plt.plot(array_b[i_max], array_c[i_max], c='black', marker='o')
+    for i in range(1, i_min):
+        if (math.fmod(i, 4) == 0):
+            plt.plot(array_b[i], array_c[i], c='black', marker='o')
+    ax.set_xlim(b - 1.2, b + 1.2)
+    ax.set_ylim(c - 2.2, c + 2.2)
+    plt.title('Contour plot of surface E')
+
+    # Print E (printE) with array_b in [2.7, 3.3], array_c in [4.4, 5.6]
+    printE = np.zeros([10, 10])
+    printE = E[10:23, 10:23]
+    plt.figure()
+    plt.plot(printE)
+    plt.title('printE')
 
 
 def main():
     s1 = get_row(const.a, const.b)
     s2 = get_row(const.a, const.b, const.c)
     s3 = get_row(const.a, const.b, const.c, const.d)
-    without_noize(s1, s2, s3)
-    with_noise(s1, s2, s3)
-
+    # without_noize(s1, s2, s3)
+    # with_noise(s1, s2, s3)
+    exponential()
 
 
 def get_noised_row(s):
